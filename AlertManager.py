35dfddb8e544edf504f52b7280dbc1ec1100a036
RohitKumar251091 <44572059+RohitKumar251091@users.noreply.github.com>
2019-03-11 20:52:12 +0530

    Update AlertManager.py

--- AlertManager.py
+++ AlertManager.py
@@ -3,13 +3,13 @@ import smtplib
 threshold = 30
 
 
-def mail_dispatch():
+def mail_dispatch(data):
     # creates SMTP session
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("4rohit25@gmail.com", "XXXXXXXXX")
-    subject = "Temprature exceed"
-    body = "Hello this is for test"
+    s.login("4rohit25@gmail.com", "XXXXXXXXXXX")
+    subject = "TEMPRATURE ALERT"
+    body = "Current Temprature is " + str(data) + " which is greater than threshold limit i.e " + str(threshold)
     message = 'Subject: {}\n\n{}'.format(subject, body)
     s.sendmail("4rohit25@gmail.com", "rohitcheeku25@gmail.com", message)
     s.quit()
@@ -17,4 +17,4 @@ def mail_dispatch():
 
 def validator(data):
     if float(data) > float(threshold):
-        mail_dispatch()
+        mail_dispatch(data)
