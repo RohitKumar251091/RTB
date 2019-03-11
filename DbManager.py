68261b8c4bc91ef5ed0a79cbfc58bfb7b44ee341
RohitKumar251091 <44572059+RohitKumar251091@users.noreply.github.com>
2019-03-11 20:40:49 +0530

    Update DbManager.py

--- DbManager.py
+++ DbManager.py
@@ -1,5 +1,6 @@
 import pymongo
 import json
+from AlertManager import validator
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["RTB"]
@@ -8,7 +9,7 @@ db = client["RTB"]
 def sensor_data_handler(msg):
     collection = db["TEMPRATURE"]
     data = str(msg.payload.decode("utf-8", "ignore"))
-    data["TOPIC"] = str(mag.topic)
     file_data = json.loads(data)
     collection.insert_one(file_data)
     client.close()
+    validator(float(file_data["TEMPRATURE"]))
